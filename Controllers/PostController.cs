aaf57c690eec59b38b9ae8a755be05050b5a6648
majuh <majuhlins76@gmail.com>
2025-02-05 13:14:43 -0400

    Update PostController

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -3,11 +3,35 @@ using ProjetoPABD.DTOs;
 using ProjetoPABD.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 namespace ProjetoPABD.Controllers
 {
+    [ApiController]
+    [Route("posts")]
     public class PostController : Controller
     {
-        
+        private readonly AppDbContext _context;
+
+        public PostController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var listaPosts = await _context.Post.ToListAsync();
+                return Ok(listaPosts);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
 
     }
 }
